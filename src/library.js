08814ae6e46d44b1882d51cff04d929925f331ba
dmshikoff <dmshikoff@gmail.com>
2018-04-25 18:24:49 -0700

    create decks done. working on adding cards to decks

--- src/library.js
+++ src/library.js
@@ -14,6 +14,36 @@
     })
 });
 
+const decksButton = document.querySelector('.deck-button')
+const libraryButton = document.querySelector('.library-button')
+const libraryCont = document.querySelector('.library-cont')
+const decksCont = document.querySelector('.decks-cont')
+const newDeckCont = document.querySelector('.new-deck-cont')
+
+decksButton.addEventListener('click', event => {
+  libraryCont.classList.add('hide')
+  decksCont.classList.remove('hide')
+  newDeckCont.classList.add('hide')
+
+  request(`/auth/token`)
+  .then(response => {
+
+   return request(`/users/${response.data.id}/decks`)    
+  })
+  .then(response => {
+
+      response.data.data.forEach(deck => {
+        renderMyDecks(deck.id, deck.name)
+      })
+  })
+})
+
+libraryButton.addEventListener('click', event => {
+  libraryCont.classList.remove('hide')
+  decksCont.classList.add('hide')
+  newDeckCont.classList.add('hide')
+})
+
 const librarySearch = document.querySelector('.library-search-form')
 librarySearch.addEventListener('submit', event => {
   event.preventDefault()
@@ -91,23 +121,23 @@ addCardForm.addEventListener('submit', event => {
 
   axios.get(`https://api.magicthegathering.io/v1/cards${queryText}`)
     .then(response => {
-      
+
       cards = response.data.cards.map(card => {
         return returnDisplay(card)
       })
-      console.log(cards)
 
-      cards.reduce((acc, ele) => {
-          acc.push({
-            img: ele.img,
-            id: ele.multiverseId
-          })
-          return acc
-        }, [])
-        .forEach(ele => {
-          const putArray = []
-          display.appendChild(createImgElementInModal(ele))
-        })
+      // cards.reduce((acc, ele) => {
+      //     acc.push({
+      //       img: ele.img,
+      //       id: ele.multiverseId
+      //     })
+      //     return acc
+      //   }, [])
+
+      cards.forEach(ele => {
+        const putArray = []
+        display.appendChild(createImgElementInModal(ele))
+      })
       const cardArray = document.querySelectorAll('.library-img')
     })
 })
@@ -122,9 +152,11 @@ addCard.addEventListener('click', event => {
   request('/auth/token')
     .then(response => {
 
-      request(`/users/${response.data.id}/cards`, 'post', selectedCards)
+      return request(`/users/${response.data.id}/cards`, 'post', selectedCards)
     })
-
+    .then(response => {
+      selectedCards = []
+    })
 })
 
 const removeCardForm = document.querySelector('.remove-card-form')
@@ -179,16 +211,16 @@ removeCard.addEventListener('click', event => {
     })
 })
 
-const selectedCards = [];
+let selectedCards = [];
 
-function createImgElementInModal(string) {
+function createImgElementInModal(obj) {
   let img = document.createElement('img')
-  img.setAttribute('src', string.img)
-  img.setAttribute('data-id', string.id)
+  img.setAttribute('src', obj.img)
+  img.setAttribute('data-id', obj.id)
   img.classList.add('library-img')
   img.addEventListener('click', event => {
     img.classList.add('selected')
-    selectedCards.push(cards.find(obj => obj.multiverseId == event.target.getAttribute('data-id')))
+    selectedCards.push(obj)
   })
   return img
 }
@@ -270,10 +302,134 @@ function createImgElement(string) {
   display.appendChild(img)
 }
 
+// function createImgElementInModal(string){
+//   const display = document.querySelector('.add-card-to-deck-modal-body')
+//   let img = document.createElement('img')
+//   img.setAttribute('src', string)
+//   img.classList.add('library-img')
+//   display.appendChild(img)
+// }
+
 function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild)
   }
 }
 
-const newDeck = document.querySelector('#new-deck')
+const newDeckForm = document.querySelector('.new-deck-name-form')
+newDeckForm.addEventListener('submit', event => {
+  event.preventDefault()
+
+
+  request('/auth/token')
+    .then(response => {
+
+      request(`/users/${response.data.id}/decks`, 'post', {
+          name: event.target.cardNameValue.value
+        })
+        .then(response => {
+
+          const newDeckNameTitle = document.querySelector('.new-deck-name-title')
+          newDeckNameTitle.innerHTML = event.target.cardNameValue.value
+          libraryCont.classList.add('hide')
+          decksCont.classList.add('hide')
+          newDeckCont.classList.remove('hide')
+
+          renderMyDecks(event.target.cardNameValue.value)
+
+          $('#addDeckModal').modal('hide')
+        })
+    })
+})
+
+function renderMyDecks(id, name){
+  const decksDisplay = document.querySelector('.all-decks-display')
+  const img = document.createElement('img')
+  const deckDiv = document.createElement('div')
+  const deckName = document.createElement('h2')
+
+  // deckDiv.classList.add('d-inline-flex')
+  deckName.innerHTML = name
+  deckDiv.classList.add('library-img')
+  deckDiv.setAttribute('data-name', name)
+  deckDiv.setAttribute('data-id', id)
+  deckDiv.classList.add('size')
+  img.setAttribute('src', '../img/Magic_Card_Back.png')
+  deckDiv.appendChild(deckName)
+  deckDiv.appendChild(img)
+  decksDisplay.appendChild(deckDiv)
+}
+
+
+
+const addCardToDeckForm = document.querySelector('.library-deck-form')
+addCardToDeckForm.addEventListener('submit', event => {
+  event.preventDefault()
+  const display = document.querySelector('.add-card-to-deck-modal-body')
+  empty(display)
+  let searchQuery = {}
+  if (event.target.createDeckRadios.value === 'name' && event.target.AddDeckSearchTerm.value) {
+    searchQuery.name = textQueryCreator(event.target.AddDeckSearchTerm.value)
+  }
+  if (event.target.createDeckRadios.value === 'type' && event.target.AddDeckSearchTerm.value) {
+    searchQuery.type = textQueryCreator(event.target.AddDeckSearchTerm.value)
+  }
+  if (event.target.createDeckRadios.value === 'subtype' && event.target.AddDeckSearchTerm.value) {
+    searchQuery.subtype = textQueryCreator(event.target.AddDeckSearchTerm.value)
+  }
+  if (event.target.createDeckRadios.value === 'text' && event.target.AddDeckSearchTerm.value) {
+    searchQuery.text = textQueryCreator(event.target.AddDeckSearchTerm.value)
+  }
+  if (event.target.white.checked === true) {
+    searchQuery.white = true
+  }
+  if (event.target.blue.checked === true) {
+    searchQuery.blue = true
+  }
+  if (event.target.black.checked === true) {
+    searchQuery.black = true
+  }
+  if (event.target.red.checked === true) {
+    searchQuery.red = true
+  }
+  if (event.target.green.checked === true) {
+    searchQuery.green = true
+  }
+  if (event.target.colorless.checked === true) {
+    searchQuery.colorless = true
+  }
+  let queryText = `?`
+  for (let key in searchQuery) {
+    const keyValueText = `${key}=${searchQuery[key]}`
+    queryText += `${keyValueText}&`
+  }
+  queryText = queryText.slice(0, queryText.length - 1)
+
+  request('/auth/token')
+    .then(response => {
+      request(`/users/${response.data.id}/cards${queryText}`)
+        .then(data => {
+          let cards = data.data
+          cards.reduce((acc, ele) => {
+            acc.push({
+              img: ele.img
+            })
+            return acc
+          }, [])
+          cards.forEach(ele => {
+
+            display.appendChild(createImgElementInModal(ele))
+          })
+        })
+    })
+})
+
+// const addCardToDeck = document.querySelector('.card-add-to-deck')
+// addCardToDeck.addEventListener('click', event => {
+
+//   request(`/auth/token`)
+//     .then(response => {
+//       request(`/users/${response.data.id}/decks/`)
+//     })
+// })
+
